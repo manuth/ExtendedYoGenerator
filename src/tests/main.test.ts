dd0f3e5adbc43d43607e86021a5093268cbf14b9
Manuel Thalmann <m@nuth.ch>
2018-11-25 20:41:11 +0100

    Initialize an npm-module

---
+++ src/tests/main.test.ts
@@ -0,0 +1,13 @@
+import * as assert from "assert";
+
+suite(
+    "ExtendedYoGenerator",
+    () =>
+    {
+        test(
+            "Example...",
+            () =>
+            {
+                assert.strictEqual(1, 1);
+            });
+    });
