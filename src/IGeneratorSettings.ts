f8d47d54d861d5fef82697cabc7d56f8d51a88d5
Manuel Thalmann <m@nuth.ch>
2019-08-05 23:43:10 +0200

    Remove the unnecessary indexer

--- src/IGeneratorSettings.ts
+++ src/IGeneratorSettings.ts
@@ -4,7 +4,7 @@ import { GeneratorSetting } from "./GeneratorSetting";
 /**
  * Represents settings of a generator.
  */
-export interface IGeneratorSettings extends Answers
+export interface IGeneratorSettings
 {
     /**
      * Gets or sets the components to install.
