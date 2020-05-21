01b1a02b83f3b6f5d4f7a72e087c89c85162ae99
Manuel Thalmann <m@nuth.ch>
2020-05-21 17:41:27 +0200

    Update all dependencies

--- src/Generator.ts
+++ src/Generator.ts
@@ -1,4 +1,4 @@
-import chalk from "chalk";
+import chalk = require("chalk");
 import { ChoiceCollection, Separator } from "inquirer";
 import Path = require("path");
 import PkgUp = require("pkg-up");
