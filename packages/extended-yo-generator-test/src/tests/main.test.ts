17eba402854ff009764dda3cb6cede0b95ebc6fd
Manuel Thalmann <m@nuth.ch>
2020-07-09 15:13:24 +0200

    Add tests for the `extended-yo-generator-test` package

---
+++ packages/extended-yo-generator-test/src/tests/main.test.ts
@@ -0,0 +1,13 @@
+import Assert = require("assert");
+
+suite(
+    "ExtendedYoGeneratorTest",
+    () =>
+    {
+        test(
+            "Example",
+            () =>
+            {
+                Assert.strictEqual(1, 1);
+            });
+    });
