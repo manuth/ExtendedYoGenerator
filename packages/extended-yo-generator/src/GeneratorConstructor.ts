b8322d8d8cd443715b017500704a7d43fd53b249
Manuel Thalmann <m@nuth.ch>
2021-06-28 01:54:48 +0200

    Add a separate class for constructors

--- packages/extended-yo-generator/src/GeneratorConstructor.ts
+++ packages/extended-yo-generator/src/GeneratorConstructor.ts
@@ -1,6 +1,7 @@
+import { Constructor } from "./Constructor";
 import { Generator } from "./Generator";
 
 /**
  * Represents a component for constructing generators.
  */
-export type GeneratorConstructor<TGenerator extends Generator<any, any> = Generator<any, any>> = new (...params: any[]) => TGenerator;
+export type GeneratorConstructor<TGenerator extends Generator<any, any> = Generator<any, any>> = Constructor<TGenerator>;
